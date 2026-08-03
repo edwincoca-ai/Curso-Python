965074a9e766d33d02abcc3d4955ece7db3a50af
Edwin Antonio Coca Navarro <edwinantoniococanavarro@gmail.com>
2026-08-02 22:52:17 -0600

    cadenas ejemplo

--- cadenas.py
+++ cadenas.py
@@ -11,3 +11,32 @@ funcionamiento de programa: (opciones)
 # subscripting e indexado 
 
 texto = "python"
+
+print(texto[0])
+print(texto[5])
+print(texto[-1])
+print(texto[-6])
+
+# print([6]) # error! No podemos acceder a una posicion que no existe 
+# print([-7]) # error! No podemos acceder a una posicon que no existe 
+
+letra = texto[0]
+print(letra) 
+
+texto_compuesto = letra + texto[1]  #concatenacion 
+print(texto_compuesto)
+
+################################################################################################################
+
+# Slicing o Substringing 
+texto = "python"
+
+print(texto[0:3])
+print(texto[0:-3])
+print(texto[0:-2])
+print(texto[2:])
+print(texto[:3])
+
+print(texto[-3::-1])
+
+print(texto[::-1])
