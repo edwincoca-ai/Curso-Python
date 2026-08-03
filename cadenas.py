eb3e3645062a59f0ad4d3de110caabebad00fd18
Edwin Antonio Coca Navarro <edwinantoniococanavarro@gmail.com>
2026-08-02 23:59:35 -0600

    cadenas

--- cadenas.py
+++ cadenas.py
@@ -26,7 +26,7 @@ print(letra)
 texto_compuesto = letra + texto[1]  #concatenacion 
 print(texto_compuesto)
 
-################################################################################################################
+###########################################################################################
 
 # Slicing o Substringing 
 texto = "python"
@@ -40,3 +40,25 @@ print(texto[:3])
 print(texto[-3::-1])
 
 print(texto[::-1])
+print(texto[1:50])
+print(texto[2:2])
+
+##########################################################################################
+# Cadenas y Formatos
+
+texto = "Hola mundo! Buenastardes"
+print(texto.lower())
+print(texto.upper())
+print(texto.capitalize())
+print(texto.title())
+print(texto.swapcase())
+# texto = texto.upper()
+# print(texto)
+
+print('{} + {} = {}'.format(1, 2, 1 + 2))
+print('{} + {} = {}'.format(2, 3, 2+3))
+print('{} + {} = {}'.format('hola', 'mundo', 'hola mundo'))
+print('{:.3f} + {:.4f} = {}'.format(2, 3, 2+3))
+print('{1} + {0} = {2}'.format(2, 3, 2+3))
+print('{2} + {0} = {1}'.format('hola', 'mundo', 'hola mundo'))
+print('{:d} = {:b} = {:o} = {:x}'.format(15, 15, 15, 15))
